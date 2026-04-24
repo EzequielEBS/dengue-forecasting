34b6f703464c8996b098a62374e62c1af8f3ee14
Ezequiel Braga <ezequiel.braga.santos@gmail.com>
2026-04-23 22:39:59 -0300

    add code to evaluate capital cities results

--- code/capital_cities/eval_models.r
+++ code/capital_cities/eval_models.r
@@ -18,6 +18,7 @@ quantiles <- c(0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975)
 
 results <- pblapply(states, function(uf) {
   print(paste0("Evaluating models for ", uf))
+  data <- read_csv(paste0("data/capital_cities/dengue_climate_", uf, "_inla.csv"), show_col_types = FALSE)
   models_list <- list.files(paste0("results/capital_cities/", uf), full.names = TRUE)
   models <- lapply(models_list, qs_read)
   model_names <- gsub(paste0("results/capital_cities/", uf), "", models_list)
@@ -55,23 +56,64 @@ results <- pblapply(states, function(uf) {
     return(pred)
   })
 
+  wis <- lapply(seq_along(models), function(i) {
+    model <- models[[i]]
+    model_name <- names(models)[i]
+    if (model_name == "M8") {
+      model <- model %>% filter(horizon > 0)
+      model <- model %>% rename(
+        index = horizon,
+        window = window_index,
+        date_iniSE = date,
+        predicted_cases = pred_mean
+      )
+      quant_df <- model %>% select(starts_with("q"))
+      wis <- scoringutils::wis(
+        model$obs,
+        as.matrix(quant_df),
+        quantiles
+      ) %>% mean()
+    } else if (model_name %in% c("M0", "M1", "M2", "M3", "M4", "M5", "M6", "M7")) {
+      if (is.null(model$data_inla)) {
+        wis <- mean(sapply(model[[1]]$fit, compute_wis, quantile_level = quantiles, 
+                      data = data, outcome = "casos"))
+      } else {
+        wis <- mean(sapply(model[[1]]$fit, compute_wis, quantile_level = quantiles, 
+                      data = data, outcome = "casos"))
+      }
+    } else {
+      wis <- mean(sapply(model, function(pred) {
+        rows_filter <- (nrow(pred) - len_windows + 1):nrow(pred)
+        pred_matrix <- as.matrix(pred[rows_filter, -c(1:5)])
+        obs_values <- pred[rows_filter, "obs"]
+        scoringutils::wis(obs_values, pred_matrix, quantiles)
+      }))
+    }
+    return(wis)
+  })
+
+
   maes <- lapply(seq_along(pred), function(j) {
     pred_j <- pred[[j]]
     mae <- mean(abs(pred_j$obs - pred_j$predicted_cases))
     return(mae)
   })
 
-  best_model_index <- which.min(unlist(maes))
-  best_model_name <- names(models)[best_model_index]
+  best_model_index_mae <- which.min(unlist(maes))
+  best_model_name_mae <- names(models)[best_model_index_mae]
+  best_model_index_wis <- which.min(unlist(wis))
+  best_model_name_wis <- names(models)[best_model_index_wis]
 
   df_res <- data.frame(
     uf = uf,
-    best_model = best_model_name,
-    mae = maes[[best_model_index]]
+    best_model_mae = best_model_name_mae,
+    mae = maes[[best_model_index_mae]],
+    best_model_wis = best_model_name_wis,
+    wis = wis[[best_model_index_wis]]
   )
   return(df_res)
 })
 
 results_df <- do.call(rbind, results)
-results_df <- results_df %>% arrange(as.numeric(gsub("M", "", best_model)))
+results_df <- results_df %>% arrange(as.numeric(gsub("M", "", best_model_mae)))
 write_csv(results_df, "results/capital_cities/best_models.csv")
---
+++ code/capital_cities/plot_best_models_map.r
@@ -0,0 +1,256 @@
+library(tidyverse)
+library(sf)
+library(geobr)
+library(patchwork)
+
+best_models <- readr::read_csv(
+  "results/capital_cities/best_models.csv",
+  show_col_types = FALSE
+)
+
+print(best_models %>% arrange(best_model_wis), n=Inf)
+
+model_lookup <- tibble::tribble(
+  ~model_id, ~model_type, ~climate_group,
+  "M0",  "INLA",    "No climate variables",
+  "M1",  "INLA",    "Uses climate variables",
+  "M2",  "INLA",    "Uses climate variables",
+  "M3",  "INLA",    "Uses climate variables",
+  "M4",  "INLA",    "Uses climate variables",
+  "M5",  "INLA",    "Uses climate variables",
+  "M6",  "INLA",    "Uses climate variables",
+  "M7",  "INLA",    "No climate variables",
+  "M8",  "AR",      "No climate variables",
+  "M9",  "SARIMAX", "No climate variables",
+  "M10", "SARIMAX", "Uses climate variables",
+  "M11", "SARIMAX", "Uses climate variables",
+  "M12", "SARIMAX", "Uses climate variables",
+  "M13", "SARIMAX", "Uses climate variables"
+)
+
+plot_map <- function(model_col) {
+  best_models_plot <- best_models %>%
+    mutate(model_id = .data[[model_col]]) %>%
+    left_join(model_lookup, by = "model_id")
+
+  states_sf <- geobr::read_state(year = 2020, showProgress = FALSE)
+  states_sf <- geobr::read_state(year = 2020, showProgress = FALSE) %>%
+    dplyr::select(
+      uf = abbrev_state,
+      state_name = name_state,
+      geom
+    ) %>%
+    left_join(best_models_plot, by = "uf") %>%
+    mutate(
+      model_type = factor(model_type, levels = c("INLA", "AR", "SARIMAX")),
+      climate_group = factor(
+        climate_group,
+        levels = c("Uses climate variables", "No climate variables")
+      )
+    )
+
+  label_points <- sf::st_point_on_surface(states_sf)
+  coords <- sf::st_coordinates(label_points)
+
+  labels_df <- label_points %>%
+    sf::st_drop_geometry() %>%
+    mutate(
+      X = coords[, "X"],
+      Y = coords[, "Y"]
+    )
+
+  # Fetch capital cities spatial data
+  capitals_sf <- geobr::read_capitals(as_sf = TRUE, showProgress = FALSE)
+
+  plot_model_type <- ggplot(states_sf) +
+    geom_sf(aes(fill = model_type), color = "white", linewidth = 0.3) +
+    # geom_text(
+    #   data = labels_df,
+    #   aes(x = X, y = Y, label = uf),
+    #   size = 4,
+    #   fontface = "bold",
+    #   color = "black",
+    #   alpha = 0.7
+    # ) +
+    geom_sf(data = capitals_sf, color = "black", size = 1.5, shape = 16) +
+    # Layer the capital cities (Labels)
+    geom_sf_text(
+      data = capitals_sf, 
+      aes(label = name_muni), 
+      size = 5, 
+      color = "black",
+      nudge_y = -0.4 # Nudges the text slightly downwards so it doesn't block the point
+    ) +
+    scale_fill_manual(
+      values = c(
+        "INLA" = "#1b9e77",
+        "AR" = "#d95f02",
+        "SARIMAX" = "#7570b3"
+      ),
+      na.value = "grey85"
+    ) +
+    labs(
+      title = "",
+      fill = ""
+    ) +
+    theme_void() +
+    theme(
+      legend.position = "bottom",
+      plot.title = element_text(face = "bold", hjust = 0.5),
+      legend.text = element_text(size = 12)
+    )
+
+  plot_climate_use <- ggplot(states_sf) +
+    geom_sf(aes(fill = climate_group), color = "white", linewidth = 0.3) +
+    # geom_text(
+    #   data = labels_df,
+    #   aes(x = X, y = Y, label = uf),
+    #   size = 4,
+    #   fontface = "bold",
+    #   color = "black"
+    # ) +
+    geom_sf(data = capitals_sf, color = "black", size = 1.5, shape = 16) +
+    # Layer the capital cities (Labels)
+    geom_sf_text(
+      data = capitals_sf, 
+      aes(label = name_muni), 
+      size = 5, 
+      color = "black",
+      nudge_y = -0.4 # Nudges the text slightly downwards so it doesn't block the point
+    ) +
+    scale_fill_manual(
+      values = c(
+        "Uses climate variables" = "#2a9d8f",
+        "No climate variables" = "#e9c46a"
+      ),
+      na.value = "grey85"
+    ) +
+    labs(
+      title = "",
+      fill = ""
+    ) +
+    theme_void() +
+    theme(
+      legend.position = "bottom",
+      plot.title = element_text(face = "bold", hjust = 0.5),
+      legend.text = element_text(size = 12)
+    )
+  return(list(plot_model_type = plot_model_type, plot_climate_use = plot_climate_use))
+}
+
+# dir.create("results/capital_cities/plots", recursive = TRUE, showWarnings = FALSE)
+plots_mae <- plot_map(model_col = "best_model_mae")
+plots_wis <- plot_map(model_col = "best_model_wis")
+
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_type_", "best_model_mae", ".png")
+  ),
+  plot = plots_mae$plot_model_type,
+  width = 10,
+  height = 8,
+  dpi = 300
+)
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_type_", "best_model_wis", ".png")
+  ),
+  plot = plots_wis$plot_model_type,
+  width = 10,
+  height = 8,
+  dpi = 300
+)
+
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_climate_", "best_model_mae", ".png")
+  ),
+  plot = plots_mae$plot_climate_use,
+  width = 10,
+  height = 8,
+  dpi = 300
+)
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_climate_", "best_model_wis", ".png")
+  ),
+  plot = plots_wis$plot_climate_use,
+  width = 10,
+  height = 8,
+  dpi = 300
+)
+
+plot_model_type <- (plots_mae$plot_model_type + ggtitle("MAE")) +
+  (plots_wis$plot_model_type + ggtitle("WIS")) +
+  plot_layout(guides = "collect") & theme(legend.position = "bottom")
+plot_climate_use <- (plots_mae$plot_climate_use + ggtitle("MAE")) +
+  (plots_wis$plot_climate_use + ggtitle("WIS")) +
+  plot_layout(guides = "collect") & theme(legend.position = "bottom")
+
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_type_combined.png")
+  ),
+  plot = plot_model_type,
+  width = 16,
+  height = 8,
+  dpi = 300
+)
+ggsave(
+  filename = file.path(
+    "results/capital_cities/plots",
+    paste0("best_model_climate_combined.png")
+  ),
+  plot = plot_climate_use,
+  width = 16,
+  height = 8,
+  dpi = 300
+)
+
+low_high_mae <- c(
+  "RR",
+  "SE",
+  "SP",
+  "RS"
+)
+
+data_plot <- lapply(low_high_mae, function(uf) {
+  read_csv(paste0("data/capital_cities/dengue_climate_", uf, "_inla.csv"), show_col_types = FALSE) %>%
+    mutate(uf = uf)
+}) %>%
+  bind_rows()
+unique(data_plot$uf)
+
+# plot time series of some capitals
+plot_low_high_mae <- ggplot(data_plot, aes(x = data_iniSE, color = uf)) +
+  geom_line(aes(y = casos)) +
+  labs(title = "", x = "Epidemiological Week", y = "Number of Cases") + 
+  scale_x_date(date_labels = "%Y-W%V", date_breaks = "4 month") +
+  theme_minimal() +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1),
+        legend.title = element_blank(),
+        legend.position = "top",
+        legend.text = element_text(size = 14)
+      ) +
+  scale_color_manual(values = c("RR" = "red", 
+                                "SE" = "green", 
+                                "SP" = "orange",
+                                "RS" = "purple"),
+                      labels = c("RR" = "Boavista (RR)", 
+                                 "SE" = "Aracaju (SE)", 
+                                 "SP" = "São Paulo (SP)",
+                                 "RS" = "Porto Alegre (RS)"))
+
+ggsave(
+  filename = 
+    "results/capital_cities/plots/time_series_low_high_mae.png",
+  plot = plot_low_high_mae,
+  width = 10,
+  height = 6,
+  dpi = 300
+)
