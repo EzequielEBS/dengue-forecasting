60ee76497b8723cc73f8c06ee32717c35a79e911
Ezequiel Braga <ezequiel.braga.santos@gmail.com>
2026-05-20 09:35:12 -0300

    standardize inla output

--- code/capital_cities/fit_inla.r
+++ code/capital_cities/fit_inla.r
@@ -35,24 +35,90 @@ concluded <- list(
   "MS", 
   "MT", 
   "PA", 
-  "PB", 
-  "PE", 
-  "PI", 
-  "PR", 
-  "RJ", 
-  "RN",
-  "RO", 
-  "RR", 
+  # "PB", 
+  # "PE", 
+  # "PI", 
+  # "PR", 
+  # "RJ", 
+  # "RN",
+  # "RO", 
+  # "RR", 
   # "RS", 
-  "SC",
-  "SE",
-  "SP",
-  "TO"
+  # "SC",
+  # "SE",
+  # "SP",
+  # "TO"
 )
 
 quantiles <- c(0.025, 0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95, 0.975)
 len_windows <- 8
 
+make_fit_for_i <- function(
+  i, 
+  data, 
+  formula,
+  family,
+  quantiles = quantiles,
+  len_windows = len_windows
+) {
+  fit <- run_inla_model(data, 
+                        outcome = "casos",
+                        threshold_week = i,
+                        formula = formula, 
+                        family = family,
+                        quantiles = quantiles,
+                        len_windows = len_windows
+                      )
+  pred_quant <- fit$fit$summary.fitted.values[
+    (nrow(fit$fit$summary.fitted.values) - len_windows + 1):nrow(fit$fit$summary.fitted.values),
+    3:(ncol(fit$fit$summary.fitted.values) - 1)]
+  q_names <- paste0("q", formatC(quantiles, format = "f", digits = 3))
+  quant_df <- as_tibble(pred_quant)
+  colnames(quant_df) <- q_names
+
+  obs <- data %>% filter(epiweek > data$epiweek[i] & epiweek <= data$epiweek[i + len_windows]) %>% 
+    pull(casos)
+  dates <- data %>% filter(epiweek > data$epiweek[i] & epiweek <= data$epiweek[i + len_windows]) %>%
+    pull(data_iniSE)
+  pred_mean <- fit$fit$summary.fitted.values[
+    (nrow(fit$fit$summary.fitted.values) - len_windows + 1):nrow(fit$fit$summary.fitted.values),
+    "mean"]
+
+  out_df <- tibble(
+    time_id = (i):(i + len_windows - 1),
+    date = dates,
+    horizon = seq_len(len_windows),
+    obs = obs,
+    pred_mean = pred_mean
+  ) %>%
+    bind_cols(quant_df)
+
+  insamp_quant <- fit$fit$summary.fitted.values[
+    1:(nrow(fit$fit$summary.fitted.values) - len_windows), 
+    3:(ncol(fit$fit$summary.fitted.values) - 1)]
+  ins_time_ids <- 1:(nrow(fit$fit$summary.fitted.values) - len_windows)
+  ins_dates <- data %>% filter(epiweek <= data$epiweek[i]) %>% pull(data_iniSE)
+  ins_obs <- data %>% filter(epiweek <= data$epiweek[i]) %>% pull(casos)
+  insamp_mean <- fit$fit$summary.fitted.values[
+    1:(nrow(fit$fit$summary.fitted.values) - len_windows), 
+    "mean"]
+  n_ins <- length(ins_time_ids)
+  
+  ins_q_df <- as_tibble(insamp_quant)
+  colnames(ins_q_df) <- q_names
+
+  ins_df <- tibble(
+    time_id = ins_time_ids,
+    date    = ins_dates,
+    horizon = seq(-n_ins, -1),   # negative horizons mark insample (optional)
+    obs     = ins_obs,
+    pred_mean = insamp_mean
+  ) %>% bind_cols(ins_q_df)
+
+  combined_df <- bind_rows(ins_df, out_df) %>% arrange(time_id)
+
+  return(combined_df)
+}
 
 
 results <- lapply(states, function(uf) {
@@ -71,19 +137,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family0 <- "poisson"
   print(paste0("Fitting model 0 for ", uf))
-  results_M0 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i,
-                          formula = f0, 
-                          family = family0,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }
-  ))
+  results_M0 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f0,
+    family = family0,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 1 
   f1 <- casos ~ 1 + 
       temp_avg_8w + 
@@ -93,18 +155,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family1 <- "poisson"
   print(paste0("Fitting model 1 for ", uf))
-  results_M1 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i,
-                          formula = f1, 
-                          family = family1,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M1 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f1,
+    family = family1,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 2
   f2 <- casos ~ 1 + 
       temp_min_avg_8w + 
@@ -114,18 +173,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family2 <- "poisson"
   print(paste0("Fitting model 2 for ", uf))
-  results_M2 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate,
-                          outcome = "casos", 
-                          threshold_week = i,
-                          formula = f2, 
-                          family = family2,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M2 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f2,
+    family = family2,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 3
   f3 <- casos ~ 1 + 
       temp_max_avg_8w +
@@ -135,18 +191,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family3 <- "poisson"
   print(paste0("Fitting model 3 for ", uf))
-  results_M3 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate,
-                          outcome = "casos", 
-                          threshold_week = i,
-                          formula = f3, 
-                          family = family3,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M3 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f3,
+    family = family3,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 4
   f4 <- casos ~ 1 + 
       temp_avg_12w +
@@ -156,18 +209,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family4 <- "poisson"
   print(paste0("Fitting model 4 for ", uf))
-  results_M4 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i,
-                          formula = f4, 
-                          family = family4,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M4 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f4,
+    family = family4,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 5
   f5 <- casos ~ 1 + 
       temp_min_avg_12w +
@@ -177,18 +227,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family5 <- "poisson"
   print(paste0("Fitting model 5 for ", uf))
-  results_M5 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows),
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i, 
-                          formula = f5, 
-                          family = family5,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M5 <- pblapply(start_week:(nrow(dengue_climate) - len_windows),
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f5,
+    family = family5,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   # Model 6
   f6 <- casos ~ 1 + 
       temp_max_avg_12w +
@@ -198,18 +245,15 @@ results <- lapply(states, function(uf) {
       f(year_id, model = "iid", constr = T)
   family6 <- "poisson"
   print(paste0("Fitting model 6 for ", uf))
-  results_M6 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows), 
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i,
-                          formula = f6, 
-                          family = family6,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M6 <- pblapply(start_week:(nrow(dengue_climate) - len_windows), 
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f6,
+    family = family6,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
   f7 <- casos ~ 1 +
       f(week_id, model = "rw2", constr = T, cyclic = T,
         hyper = list(
@@ -231,64 +275,24 @@ results <- lapply(states, function(uf) {
       )
   family7 <- "nbinomial"
   print(paste0("Fitting model 7 for ", uf))
-  results_M7 <- transpose(pblapply(start_week:(nrow(dengue_climate) - len_windows), 
-  function(i) {
-    fit <- run_inla_model(dengue_climate, 
-                          outcome = "casos",
-                          threshold_week = i,
-                          formula = f7, 
-                          family = family7,
-                          quantiles = quantiles,
-                          len_windows = len_windows
-                        )
-    return(fit)
-  }))
+  results_M7 <- pblapply(start_week:(nrow(dengue_climate) - len_windows), 
+    make_fit_for_i,
+    data = dengue_climate,
+    formula = f7,
+    family = family7,
+    quantiles = quantiles,
+    len_windows = len_windows
+  ) %>% 
+    bind_rows(.id = "window_index")
 
-  pred_M0 <- get_pred(results_M0$data_inla, len_windows = len_windows)
-  pred_M1 <- get_pred(results_M1$data_inla, len_windows = len_windows)
-  pred_M2 <- get_pred(results_M2$data_inla, len_windows = len_windows)
-  pred_M3 <- get_pred(results_M3$data_inla, len_windows = len_windows)
-  pred_M4 <- get_pred(results_M4$data_inla, len_windows = len_windows)
-  pred_M5 <- get_pred(results_M5$data_inla, len_windows = len_windows)
-  pred_M6 <- get_pred(results_M6$data_inla, len_windows = len_windows)
-  pred_M7 <- get_pred(results_M7$data_inla, len_windows = len_windows)
-
-  mae_M0 <- mean(abs(pred_M0$obs - pred_M0$predicted_cases))
-  mae_M1 <- mean(abs(pred_M1$obs - pred_M1$predicted_cases))
-  mae_M2 <- mean(abs(pred_M2$obs - pred_M2$predicted_cases))
-  mae_M3 <- mean(abs(pred_M3$obs - pred_M3$predicted_cases))
-  mae_M4 <- mean(abs(pred_M4$obs - pred_M4$predicted_cases))
-  mae_M5 <- mean(abs(pred_M5$obs - pred_M5$predicted_cases))
-  mae_M6 <- mean(abs(pred_M6$obs - pred_M6$predicted_cases))
-  mae_M7 <- mean(abs(pred_M7$obs - pred_M7$predicted_cases))
-
-  mae_results <- data.frame(
-    file_name = c(paste0("results_M0.qs2"), 
-                  paste0("results_M1.qs2"), 
-                  paste0("results_M2.qs2"), 
-                  paste0("results_M3.qs2"), 
-                  paste0("results_M4.qs2"), 
-                  paste0("results_M5.qs2"), 
-                  paste0("results_M6.qs2"), 
-                  paste0("results_M7.qs2")),
-    mae = c(mae_M0, mae_M1, mae_M2, mae_M3, mae_M4, mae_M5, mae_M6, mae_M7)
-  )
-
-  results <- list(
-    results_M0,
-    results_M1,
-    results_M2,
-    results_M3,
-    results_M4,
-    results_M5,
-    results_M6,
-    results_M7
-  )
-
-  best_model_id <- which(mae_results$mae <= sort(mae_results$mae)[1])
-  # save the results of the best model
-  best_model <- mae_results$file_name[best_model_id]
-  best_result <- results[best_model_id]
-  qs_save(best_result,
-      file = paste0("results/capital_cities/", uf, "/", best_model))
-  })
+  # save dfs
+  write_csv(results_M0, file = paste0("results/capital_cities/", uf, "/results_M0.csv"))
+  write_csv(results_M1, file = paste0("results/capital_cities/", uf, "/results_M1.csv"))
+  write_csv(results_M2, file = paste0("results/capital_cities/", uf, "/results_M2.csv"))
+  write_csv(results_M3, file = paste0("results/capital_cities/", uf, "/results_M3.csv"))
+  write_csv(results_M4, file = paste0("results/capital_cities/", uf, "/results_M4.csv"))
+  write_csv(results_M5, file = paste0("results/capital_cities/", uf, "/results_M5.csv"))
+  write_csv(results_M6, file = paste0("results/capital_cities/", uf, "/results_M6.csv"))
+  write_csv(results_M7, file = paste0("results/capital_cities/", uf, "/results_M7.csv"))
+  
+})
